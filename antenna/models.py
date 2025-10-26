e79204a4581d6a89a27928bbc6b2edd4e82c3f5b
weiwen(226) <weiwen@alum.ccu.edu.tw>
2025-10-26 22:37:58 +0800

    refactor(model): 重新調整model

--- antenna/models.py
+++ antenna/models.py
@@ -264,39 +264,6 @@ class BinarizeSTE(Function):
         mask, = ctx.saved_tensors
         return grad_output * mask  # 只保留 mask 區域的梯度
 
-
-
-class HFSSNet(nn.Module):
-
-    def __init__(self, num_pattern_pixel = 625, num_response:tuple = (3, 17)):
-        super(HFSSNet, self).__init__()
-        self.num_response = num_response
-        self.num_pattern_pixel = num_pattern_pixel
-
-        self.fc_patch = nn.Sequential(
-            nn.Linear(num_pattern_pixel, 2048),
-            nn.PReLU(),
-            nn.Linear(2048, 1024),
-            nn.PReLU(),
-            nn.Linear(1024, 512),
-            nn.PReLU(),
-            nn.Linear(512, 128),
-            nn.PReLU(),
-            nn.Linear(128, 64),
-            nn.PReLU(),
-            nn.Linear(64, num_response[0]*num_response[1])
-        )
-        self.to(config.device)
-        
-    def __repr__(self):
-        return f"{self.__class__.__name__}(num_pattern_pixel={self.num_pattern_pixel}, num_response={self.num_response}"
-    
-    def forward(self, input):
-        x = self.fc_patch(input)
-        x = x.reshape(self.num_response)
-        return x
-    
-
 # %%
 from .functions import gumbel_sinkhorn_rectangular
 class SPGEN(nn.Module, Generic[CallableParam]):
